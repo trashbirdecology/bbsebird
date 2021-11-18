afffe11b4b058e4cb87fcef0d667ec610ec952dd
Jessica Burnett <jessicaleighburnett@gmail.com>
2021-11-18 11:26:45 -0700

    ebird fxns nowhere near beta

---
+++ R/munge_ebird.R
@@ -0,0 +1,52 @@
+#' @title Munge eBird Data Using Auk
+#' @description Wrapper for handling eBird data using package Auk.
+#' @param dir.ebd.in Directory/directories for where the __unpacked__ EDB data file(s) are stored.
+#' @param dir.ebird.out Where to store the munged versions of the data.
+#' @export munge_ebird
+
+
+munge_ebird <- function(dir.ebird.in, dir.ebird.out, interest.species=NULL, interest.spatial=NULL){
+
+  ebird.fns <- tolower(list.files(dir.ebird.in, full.names=TRUE))
+  ebird.fns.spp.subset <- ebird.fns[str_detect(ebird.fns, pattern=paste(interest.species, collapse="|"))]
+
+  ebird.fns.in <- NULL
+  if(!is.null(interest.spatial)){
+    ebird.fns.spp.state.subset <- ebird.fns.spp.subset[str_detect(ebird.fns.spp.subset,
+                                                                  pattern=paste(tolower(interest.spatial), collapse="|"))]
+  if(!length(ebird.fns.spp.state.subset)>0)stop(paste0("No files matching the regions specified in `interest.spatial` were found in directory ( ",dir.ebird.in,
+                                                       " Please verify the files are unpacked in the correct directory, or remove argument `interest.spatial` if the directory includes the entire dataset."))
+    else(ebird.fns.in<-c(ebird.fns.in, ebird.fns.spp.state.subset))
+
+    }
+  if(is.null(interest.spatial)){
+    ctry.ind=c(paste0("ebd_ca_", interest.species), paste0("ebd_us_", interest.species))
+    # find files for Canada and US if no states are specified
+    ebird.fns.spp.ctry.subset <- ebird.fns.spp.subset[str_detect(ebird.fns.spp.subset, pattern=paste(tolower(ctry.ind), collapse="|"))]
+    #remove .zip and .tar files
+    ebird.fns.spp.ctry.subset <- ebird.fns.spp.ctry.subset[!str_detect(ebird.fns.spp.ctry.subset, pattern=".zip|.tar")]
+
+    ebird.fns.in <- c(ebird.fns.in, ebird.fns.spp.ctry.subset)
+  }
+
+## Import the ebird files
+  ## Auk seems REALLY slow, so for now I am going to avoid it..but i do need to follow-up on that..
+ebird.fns.in <- unique(ebird.fns.in) # ensure no duplicate filenames.
+
+ebird.data <- list()
+# This takes ~15-20 minutes, so import the .RDS if one is available.
+ebird.data$events <- read.csv("C:/Users/jburnett/OneDrive - DOI/research/cormorants/dubcorm-data-backup/ebird/ebd_sampling_relSep-2021.txt", sep="\t")
+
+s.read=Sys.time()
+for(i in seq_along(ebird.fns.in)){
+  message("importing file ", i, " of ", length(ebird.fns.in))
+  ebird.data[[i]] <- read.csv(ebird.fns.in[i], sep="\t")
+}
+ebird.data <- bind_rows(ebird.data)
+s.read=s.read-Sys.time()
+s.auk=Sys.time()
+s.auk=s.auk-Sys.time()
+
+hist(as.POSIXct.Date(samp$OBSERVATION.DATE))
+
+}
--- data-raw/data-ebird.R
+++ data-raw/data-ebird.R
@@ -1,35 +1,34 @@
-#################################################################
-## The ebird data datasets are pretty large, so will need to be updated manually.
-## The EBD must be manually downloaded with permission through ebird.org/data/request
-## Therefore, this function relies on a locally-stored version of the eBird data.
-## For this modeling framework we need zero-filled data, which requires both the EBD and the Sampling Events
-##
-## This function allows for one species and one or more regions (e.g. CA and US).
-### FOR NOW, HOWEVER, I AM JUST WRITING THIS FUNCTION TO WORK FOR THE US-STATES IN THE
-### PACIFCI FLWAY
-library(lubridate)
-library(sf)
-library(gridExtra)
-
-library(tidyverse)
-
-
-#### IMPORTANT:::SPECIFICATIONS FOR THIS EXAMPLE --TO BE CHANGED EVENTUALLY TO BE GENERALIZABLE
-states<-paste0("US-", c("OR", "CA","WA", "ID", "AZ", "NV"))
-
-# Unpack ebird data for a given species -------------------------------------------
-unpack_ebird(spp.ind="doccor") # unpack the .zip files
-
+# #################################################################
+# ## The ebird data datasets are pretty large, so will need to be updated manually.
+# ## The EBD must be manually downloaded with permission through ebird.org/data/request
+# ## Therefore, this function relies on a locally-stored version of the eBird data.
+# ## For this modeling framework we need zero-filled data, which requires both the EBD and the Sampling Events
+# ##
+# ## This function allows for one species and one or more regions (e.g. CA and US).
+# ### FOR NOW, HOWEVER, I AM JUST WRITING THIS FUNCTION TO WORK FOR THE US-STATES IN THE
+# ### PACIFCI FLWAY
+# library(lubridate)
+# library(sf)
+# library(gridExtra)
+#
+# library(tidyverse)
+#
+#
+# #### IMPORTANT:::SPECIFICATIONS FOR THIS EXAMPLE --TO BE CHANGED EVENTUALLY TO BE GENERALIZABLE
+# states<-paste0("US-", c("OR", "CA","WA", "ID", "AZ", "NV"))
+#
+# # Unpack ebird data for a given species -------------------------------------------
+# unpack_ebird(spp.ind="doccor") # unpack the .zip files
+#
 # Define reference to local files -----------------------------------------
 # local filenames
-fns <- list.files("data-raw/ebird-data", full.names = TRUE)
+# fns <- list.files("data-raw/ebird-data", full.names = TRUE)
 f_ebd <-grep(".txt", grep("ebd_US_doccor_rel" ,fns, value = TRUE), value=TRUE) # paige's f_ebd
 f_samp <-grep(".txt", grep("sampling" ,fns, value = TRUE), value=TRUE) # paige's f_ebd
 
 # Set up output files
 ebd.out <- "data-raw/ebird-data/ebd_dcco_filtered.txt"
 checklist.out <- "data-raw/ebird-data/checklist_dcco_filtered.txt"
-
 
 
 # Define filters ----------------------------------------------------------
