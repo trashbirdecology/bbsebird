ba4b6b46894a69588ca891bf9fbdc5aad26df316
Jessica Burnett <jessicaleighburnett@gmail.com>
2021-11-19 18:44:14 -0700

    vroom it is

--- R/get_ebird_sampling_events.R
+++ R/get_ebird_sampling_events.R
@@ -1,7 +1,7 @@
 #' @title
 #' @description The eBird sampling events data is quite large, often maxing out available RAM even when R's memory.limit() is set to max. This function splits the sampling event data into smaller files and uses `auk` functions to import.
-#' @param fn Filename for the sampling .txt file. Either fn or dir must be specified.
-#' @param dir Directory for where the sampling event file(s) are stored. Either fn or dir must be specified.
+#' @param f_samp Filename for the sampling .txt file. Either f_samp or dir must be specified.
+#' @param dir Directory for where the sampling event file(s) are stored. Either f_samp or dir must be specified.
 #' @param dir.out Directory for where to store the subsets of the sampling events data. If not specified will default to data-local/ebird.
 #' @param n.amer Logical. If working with North American countries, significantly reduces the file sizes of the sampling events data.
 #' @param overwrite Logical. If .RDS file, "sampling_events.rds" exists in dir or dir.out, will NOT overwrite when FALSE.
@@ -9,82 +9,78 @@
 #'
 
 get_ebird_sampling_events <-
-  function(fn = NULL,
+  function(f_samp = NULL,
            dir = NULL,
-           dir.out = "data-local/ebird",
+           dir.out = "data-local/ebird/sampling-out/",
            n = 8000000,
            n.amer = TRUE,
            complete = TRUE,
            protocol.type = c("Traveling", "Stationary")
            ){
-
+# browser()
   # create directories if necessary for storing data.
   suppressWarnings(dir.create('data-local')); suppressWarnings(dir.create(dir.out))
 
-  if(is.null(fn) & is.null(dir))stop("Both `fn` and `dir` cannot be NULL. Please specify one of the arguments. ")
+  if(is.null(f_samp) & is.null(dir))stop("Both `f_samp` and `dir` cannot be NULL. Please specify one of the arguments. ")
 
-  if(!is.null(dir)){
-    fns.ebird.in=list.files(dir, full.names=TRUE)
-    fn=fns.ebird.in[!str_detect(fns.ebird.in, ".zip|.tar|.gz")]
-    fn=fn[str_detect(fn, "sampling")]
+  if(!is.null(dir) & is.null(f_samp)){
+    f_samps.ebird.in=list.files(dir, full.names=TRUE)
+    f_samp=f_samps.ebird.in[!str_detect(f_samps.ebird.in, ".zip|.tar|.gz")]
+    f_samp=f_samp[str_detect(f_samp, "sampling")]
   }
   if(is.null(dir.out)) dir.out <- dir
 
 
 # if multuiple sampling files exist let user decide which to use.
-if(length(fn)>1){
-  choice=menu(choices=c(fn), title="Multiple sampling events files detected. Please choose which to import:")
+if(length(f_samp)>1){
+  choice=menu(choices=c(f_samp), title="Multiple sampling events files detected. Please choose which to import:")
   if(choice==0|!exists("choice")){message("No choice specified. Defaulting to first in list."); choice=1}
-  fn <- fn[choice]
+  f_samp <- f_samp[choice]
   }
 
 # break sampling file up using one of two methods: count lines, or guess (risky!)
 if(is.null(n)){
-  message("Counting the number of lines in file ", fn,". This may take a couple of minutes...")
-  n=R.utils::countLines(fn, chunkSize=1000)
+  message("Counting the number of lines in file ", f_samp,". This may take a couple of minutes...")
+  n=R.utils::countLines(f_samp, chunkSize=1000)
   }
 
 
 
-  #setup parallel backend to use many processors
-  cores=parallel::detectCores()
-  cl <- parallel::makeCluster(cores[1]-2) #not to overload your computer
-  doParallel::registerDoParallel(cl)
+#setup parallel backend to use many processors
+cores=parallel::detectCores()
+cl <- parallel::makeCluster(cores[1]-2) #not to overload your computer
+doParallel::registerDoParallel(cl)
 
-  # create indices for splitting up file
-  n.lines.total = n
-  n.lines.df.max = 500000 # max nu lines to import per df at a time
-  n.dfs.out = as.integer(round(n.lines.total/n.lines.df.max)+1)
-  # create an interval for reading lines
-  interval.start = seq(2,n.lines.total, n.lines.df.max) # start at row two to avoid importing the header.
-  interval.end = interval.start+n.lines.df.max
-  # create headers
-  headers <- read.csv(fn, header=FALSE, sep="\t", skip=0, nrows = 1)#import the header..
-
-
-  foreach(i=interval.start, j=interval.end, .combine='c') %dopar% {
-    library(tidyverse)
+# create indices for splitting up file
+n.lines.df.max =50000 # max nu lines to import per df at a time
+# (n.dfs.out = as.integer(round(n/n.lines.df.max)+1))
+# create an interval for reading lines
+interval.start = seq(2,n, n.lines.df.max) # start at row two to avoid importing the header.
+# create headers
+header <- read.csv(f_samp, header=TRUE, sep="\t", nrows = 1) #import the header..# for some reason when i do nrows=0 it takes a long ass time...
+header <- names(header)
+interval.start=interval.start[1:2]
+foreach(i=interval.start, .combine='c') %dopar% {
+    library(magrittr)
+    library(dplyr)
     library(auk)
-    df = read.csv(
-      fn,
-      header = FALSE,
-      sep = "\t",
-      skip = i,
-      nrows =n.lines.df.max)
-    colnames(df) <- headers
-    ebird.events <- df %>%
+    library(vroom)
+
+
+  df = vroom(file= f_samp,delim = "\t")
+      df <- df %>%
       filter(country %in% c("United States", "Mexico", "Canada"))
-    saveRDS(ebird.events, file=paste0(dir.out, "/sampling_events_",
-                                      Sys.Date(),"_",
-                                      i,"_to_",j,
-                                      ".rds"), compress=TRUE)
+    saveRDS(df, file=paste0(dir.out, "/sampling_events_",
+                                      Sys.Date(),"_startrow_",
+                                      i,".rds"), compress=TRUE)
 
-
+  rm(df)
+  gc()
   }
 
 
 # Grab the list of shit
-samp.fns <- list.files(dir.out, pattern = "sampling_events_", full.names=TRUE)
-return(samp.fns)
+samp.f_samps <- list.files(dir.out, pattern = "sampling_events_", full.names=TRUE)
+return(samp.f_samps)
 
 } # END FUNCTION
--- runthrough.R
+++ runthrough.R
@@ -14,7 +14,9 @@ library(sp)
 library(maps)
 library(ggplot2)
 library(mapproj)
-library(bigreadr) # used in get_ebird_sampling_events to deadl with large dsamplin gevents data
+library(doParallel)
+library(data.table)
+# library(bigreadr) # used in get_ebird_sampling_events to deadl with large dsamplin gevents data
 ## not sure i need
 library(gridExtra)
 library(raster)
@@ -26,9 +28,9 @@ options(stringsAsFactors = FALSE)
 
 # Memory limits -----------------------------------------------------------
 # adjust memory limit to handle large eBird data files depending on OS
-if(.Platform$OS.type=="unix") library(unix)
-if(.Platform$OS.type=="unix") unix::rlimit_as(1e12) # prob unnecessary but doesn't hurt (probably)
-if(.Platform$OS.type=="windows") memory.limit(35000)
+# if(.Platform$OS.type=="unix") library(unix)
+# if(.Platform$OS.type=="unix") unix::rlimit_as(1e12) # prob unnecessary but doesn't hurt (probably)
+# if(.Platform$OS.type=="windows") memory.limit(35000)
 
 
 # Directory specifications and creation ------------------------------------------------
@@ -60,9 +62,28 @@ interest.species <- c("DOCCOR", "DOCCO", "DCCO", "DCCOR", "Double-crested Cormor
 interest.temporal <- 1970:2019
 include.unid <- FALSE ## Whether or not to include UNIDENTIFIED // hybrid species
 map.region <- c("Canada","USA", "Mexico") # used to create base maps and spatial grid system.
-
 # grid.size <- c(1, "deg")
 grid.size <- c(6, "km")
+
+# Munge eBird data --------------------------------------------------------
+  # The ebird data should be in directory ebird.data.in.
+    ## Functions in this section will import, munge, and save those files as .rds.
+    ## Currently, this script / package does not use Auk to manipulate the ebird data using awk.
+    ## Auk seems significantly slower than just importing as .txt and munging, at least when working with a single species
+
+## using spatial/species filters, see what files are available for import from dir.ebird.in
+fns.ebird.in <- id_ebird_files(dir.ebird.in, species=interest.species)
+# get_ebird_sampling_events(dir=dir.ebird.in)
+f_samp <- fns.ebird.in[str_detect(fns.ebird.in,"ebd_sampling_rel")]
+f_ebd  <- fns.ebird.in[str_detect(fns.ebird.in,"doccor")]
+
+auk.time=Sys.time()
+ebird_sampling <- vroom::vroom(f_samp) # about two minutes...
+ebird_observations <- vroom::vroom(f_ebd) # about two minutes...
+Sys.time()-auk.time
+
+
+
 
 # Munge BBS route shapefiles ----------------------------------------------
 bbs_routes_sldf <- munge_bbs_shapefiles(cws.routes.dir = cws.routes.dir,
@@ -75,76 +96,6 @@ if(exists("sb_items"))rm(sb_items) # id like to add an arg to grab_bbs_data that
 
 # filter by species of interest
 bbs.subset<-filter_bbs_by_species(bbs, search = interest.species)
-
-
-# Munge eBird data --------------------------------------------------------
-  # The ebird data should be in directory ebird.data.in.
-    ## Functions in this section will import, munge, and save those files as .rds.
-    ## Currently, this script / package does not use Auk to manipulate the ebird data using awk.
-    ## Auk seems significantly slower than just importing as .txt and munging, at least when working with a single species
-
-## using spatial/species filters, see what files are available for import from dir.ebird.in
-fns.ebird.in <- id_ebird_files(dir.ebird.in, species=interest.species)
-
-## use Auk to subset and import zero-filled data
-auk::auk_set_ebd_path(dir.ebird.in, overwrite = TRUE)
-
-f_samp_in <- list.files(dir.ebird.in, "ebd_sampling", full.names=TRUE)
-f_samp_in <- f_samp_in[!str_detect(f_samp_in, ".tar|.gz|.zip")]
-
-
-# make_ebird_data(fns.ebird.in, dir.ebird.in)
-
-get_ebird_sampling_events(dir=dir.ebird.in)
-#   f_ebd_in <- fns.ebird.in[i]
-#   f_ebd_out <-  paste0(dir.ebird.in,"/ebd_filtered_out_", i ,".txt")
-#   # sampling events file out
-#   f_samp_out <- paste0(dir.ebird.in,"/sampling_filtered_out_", i ,".txt")
-#   # filter and save to file the filtered data
-#
-#   samp <- auk_sampling(file=f_samp_in)
-#
-#   filters <- auk_ebd(file = f_ebd_in,
-#                      file_sampling = f_samp) %>%
-#     auk_species(species = "Double-crested Cormorant", taxonomy_version = 2021) %>%
-#     auk_country(country = c("United States", "Canada")) %>%
-#     auk_complete() %>%
-#     auk_protocol(c("Stationary", "Traveling"))
-#
-# filtered <- auk_filter(filters,
-#                        file=f_ebd_out,
-#                        file_sampling = f_samp_out,
-#                        overwrite=TRUE)
-#
-# ebd_zf <- auk_zerofill(ebd, samp)
-#
-#
-# ebird[[i]] <- read_ebd(filtered) ## takes quite a while...
-# ebird.zf[[i]] <- auk_zerofill(ebird[[i]], sampling_events = f_samp, collapse=TRUE)
-# }
-#
-# Sys.time()-auk.time
-#
-# # to produce zero-filled data, provide an EBD and sampling event data file
-# f_ebd
-# f_samp
-# filters <- auk_ebd(f_ebd, file_sampling = f_smp) %>%
-#   auk_species("Collared Kingfisher") %>%
-#   auk_time(c("06:00", "10:00")) %>%
-#   auk_complete()
-#
-# ### checklists/sampling events
-#
-# # Zero-fill
-#
-#
-
-
-# ebird <- readRDS("data-local/ebird/dcco_ebird_2020-10-13.rds")
-# ebird <- readRDS("data-local/ebird/dcco_ebird_full_2021-02-02.rds")## this one has "observer_id"
-
-
-
 
 
 # Spatial Grid System ------------------------------------------------------------
