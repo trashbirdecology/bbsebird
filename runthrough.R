b7394816cdb23bc7798cb8423b18715787a20c3f
Jessica Burnett <jessicaleighburnett@gmail.com>
2021-11-23 14:46:36 -0700

    Update runthrough.R

--- runthrough.R
+++ runthrough.R
@@ -26,6 +26,7 @@ library(rgbif)
 library(lubridate)
 options(stringsAsFactors = FALSE)
 
+devtools::load_all()
 
 # Memory limits -----------------------------------------------------------
 # adjust memory limit to handle large eBird data files depending on OS
@@ -76,46 +77,91 @@ options(stringsAsFactors = FALSE)
 bbs <- grab_bbs_data(sb_dir=dir.bbs.out) # defaults to most recent release of the BBS dataset available on USGS ScienceBase
 if(exists("sb_items"))rm(sb_items) # i need to add an arg to bbsassistant:grab_bbs_data that prevents output of sb_items...
 
+# filter by species of interest, zero-fill
+bbs.subset <- filter_bbs_by_species(list = bbs, search = interest.species, zero.fill=TRUE)
 
-# filter by species of interest
-bbs.subset <- filter_bbs_by_species(bbs, search = interest.species, zero.fill=TRUE)
-bbs.subset$routes <- bbs.subset$routes %>%
+# ## TRYING TO FIGURE OUT WHERE I AM LOSING THE ROUTES.
+# trsub=bbs.subset$routes %>% filter(StateNum %in% c("02","03","58", 2, 3, 58)) %>%
+#   distinct(CountryNum, StateNum, Route) %>%
+#   group_by(CountryNum, StateNum) %>%
+#   summarise(n())
+# tosub<- bbs.subset$observations %>% filter(StateNum %in% c("02","03","58", 2,3,58)) %>%
+#   distinct(CountryNum, StateNum, Route) %>%
+#   group_by(CountryNum, StateNum) %>%
+#   summarise(n())
+# tr=bbs$routes %>% filter(StateNum %in% c("02","03","58", 2,3,58)) %>%
+#   distinct(CountryNum, StateNum, Route) %>%
+#   group_by(CountryNum, StateNum) %>%
+#   summarise(n())
+# to<- bbs$observations %>% filter(StateNum %in% c("02","03","58", 2,3,58)) %>%
+#   distinct(CountryNum, StateNum, Route) %>%
+#   group_by(CountryNum, StateNum) %>%
+#   summarise(n())
+# tr
+# trsub
+# to
+# tosub
+
+bbs.subset$observations <- bbs.subset$observations %>%
 # keep only Canada and US (excluding HI and AK)
   filter(CountryNum %in% c(124, 840))  %>%
   # Keep only RPID=101
-  filter(RouteTypeID==1)
+  filter(RPID==101)
 
-# remove HI and AK
+
+# remove HI and AK from the bbs.subset dataset
 data(region_codes) # region codes from bbsAssistant package.
-region_codes.subset <- region_codes %>% filter(CountryNum %in% c(124, 840))
-bbs.subset$routes <- bbs.subset$routes %>%
-  filter(!StateNum %in% region_codes$StateNum[tolower(region_codes.subset$State) %in% c("Alaska", "Canada")])
+region_codes.subset <- region_codes %>%
+  filter(CountryNum %in% c(124, 840)) # keep US and CAN only
+# have to split up the state num and country num process b/c of Mexico's character issues.
+statenums.remove <- region_codes.subset$StateNum[tolower(region_codes.subset$State) %in% c("alaska", "hawaii")]
+region_codes.subset <- region_codes.subset %>%
+  filter(!StateNum %in% statenums.remove) # remove states specified above US and CAN only
+
+bbs.subset$observations <- bbs.subset$observations %>%
+  filter(!StateNum %in% statenums.remove)
+
+## Finally, remove all the unnecessary routes in bbs$routes
+bbs.subset$routes <- bbs.subset$routes %>% filter(RTENO %in% bbs.subset$observations$RTENO)
+
+## A TEST
+if(!all(bbs.subset$routes$RTENO %in% bbs.subset$observations$RTENO)) stop("something is wrong use traceback()")
 
 
 # Munge BBS route shapefiles ----------------------------------------------
 bbs_routes_sldf <- munge_bbs_shapefiles(cws.routes.dir = cws.routes.dir,
                                         usgs.routes.dir = usgs.routes.dir,
                                         proj.target = "USGS")
-# need to troubleshoot these. ## will need to go back and double check this..
-(missing.routes <- bbs.subset$routes[which(!bbs.subset$routes$Route %in% bbs_routes_sldf$Route),])
-td=tempdir(); write.csv(missing.routes, paste0(td,'/missingroutes.csv'))
+# Keep only the routes that are in the observations data frame
+  ## i.e. remove the unwanted routes by spatial filter and, if zero.fill=FALSE, also by species.
+bbs_routes_sldf.subset <- bbs_routes_sldf[bbs_routes_sldf@data$RTENO %in% bbs.subset$observations$RTENO,]
+  # which routes are missing from the spatial shapefile layer
 
-
-
-
+#### PROBLEMS TO SOLVE: MISSING ROUTES
+bbs.subset$routes[which(!bbs.subset$routes$RTENO %in% bbs_routes_sldf.subset$RTENO), ] %>%
+  distinct(RTENO, .keep_all = TRUE) %>%
+  group_by(CountryNum, StateNum) %>%
+  summarise(n()) %>% left_join(region_codes) %>%
+  write.csv('missingroutesbyregion.csv')
+setdiff(unique(bbs.subset$observations$RTENO),
+        unique(bbs_routes_sldf@data$RTENO)) %>%  ## which of A are not in B
+  write.csv('missingroutes.csv')
 
 # Munge eBird data --------------------------------------------------------
-devtools::load_all()
-
 # Get the list of potential files for import. This will be used in get_ebird()
 (fn_zf <- list.files(dir.ebird.out, "rds"))
 fns <- id_ebird_files(dir.ebird.in = dir.ebird.in)
 ebd_zf <- get_zerofilled_ebird(fns, overwrite=FALSE)
 
-# Spatial Grid System ------------------------------------------------------------
+# force ebird to a spatial layer
+coordinates(ebd_zf) ~ c(ebd_zf$longitude, ebd_zf$latitude)
+
+
+
+# Creata a spatial grid ------------------------------------------------------------
 ## I think i want to make this a function...
 
-## create map data for N. America
+## initialize map data for N. America
 for(i in seq_along(map.region)){
  if(i==1){map.df<-list()}
  map.df[[i]] <- map_data("world", region=map.region[i])
@@ -136,5 +182,6 @@ xy <- expand.grid(x = n.amer$long, y = n.amer$lat)
 plot(xy)
 
 extent.bbs <- sp::bbox(bbs_routes_sldf)
-extent.bbs <- sp::bbox(bbs_routes_sldf)
+
+extent.bbs <- sp::bbox(ebd_zf)
 
