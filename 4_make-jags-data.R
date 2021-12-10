9668f9abf41569e6b5f5abc9d8c2568b4049ecd1
Jessica Burnett <jessicaleighburnett@gmail.com>
2021-12-10 11:55:22 -0700

    Update 4_make-jags-data.R

--- 4_make-jags-data.R
+++ 4_make-jags-data.R
@@ -8,9 +8,9 @@ fns <- c("bbs.rds", "ebird.rds", "grid.rds")
 if(all(fns %in% list.files(dir.jags))){
   cat("importing the munged bbs, ebird and grid sf objects.")
   fns=list.files(dir.jags, full.names = TRUE)
-  bbs <- readRDS(fns[str_detect(fns, "bbs_spat")])
+  bbs <- readRDS(fns[str_detect(fns, "bbs")])
   grid <- readRDS(fns[str_detect(fns, "grid")])
-  ebird <- readRDS(fns[str_detect(fns, "ebird_spat")])
+  ebird <- readRDS(fns[str_detect(fns, "ebird")])
 }else{
   cat("Building the BBS and eBird data and spatial objects. Sit back and relax......\nor be worried for errors halfway through")
   source("3_make-bird-data.R")
@@ -44,6 +44,69 @@ jags <- list(
   Xp = matrix(NA),
   XY = data.frame(x=NULL, y=NULL, layer=NULL, covar1=NULL)
 )
+# PAIGE'S JDAT
+#   nSites = nrow(XY), # Number of grid cells in study area
+#
+#   ## Doublecheck that phab is in the correct order... ##
+#   # Proportion of grid cell covered by freshwater. Scaled to mean 0, unit
+#   # variance.
+#   #pwet = XY.filt[,"pwet"],    # Matrix (ngridCells x nYears).
+#
+#   # Vector (nSites).  Proportion of grid cell covered by freshwater.
+#   # Scaled to mean 0, unit variance.
+#   phab = XY[,"phab"],
+#
+#   ## BBS route level data ##
+#   BBSr = bbs.jags.NY$BBSr, # Matrix (nyears x nroutes) with DCCO count data for
+#   # each route, each year
+#   Xp = bbs.jags.NY$wind.z, # Matrix (nyears x nroutes) with wind data for each
+#   # route x year
+#   nov = bbs.jags.NY$nov,   # Matrix (nyears x nroutes) with indicator for
+#   # whether an observer was brand new in a given year (1) or not (0) data for
+#   # each route x year
+#   propStops = bbs.jags.NY$propStops, # Array (nyears x nroutes x ngridcells)
+#   # with effort data for each route x year combination. This is the proportion
+#   # of a route that falls within each of our grid cells
+#
+#   nYearsB = nrow(bbs.jags.NY$BBSr), # Scalar indicating number of years with BBS data
+#   Brsite = bbs.jags.NY$Brsite,      # Matrix (nyears x nroutes) indicating
+#   # routes run in a given year
+#   nRoutes = bbs.jags.NY$nRoutes,    # Vector (nyears) indicating total number of
+#   # routes run each year
+#   Brgrid = bbs.jags.NY$Brgrid,      # Array (nroutes x ngridcells x nyears)
+#   # Indicates the identity of grid cells that a route crosses each year
+#   nGridswithRoutes = bbs.jags.NY$nGridswithRoutes, # Matrix (nroutes x nyears)
+#   # Indicates maximum number of grid cells that a route crosses each year
+#
+#   ## eBird Data ##
+#   E = ebird.jags.NY$ebird_counts, # Array of DCCO ebird data (nGridcells x nyears
+#   # x nchecklists)
+#   nSitesSurveyed = ebird.jags.NY$nSitesSurveyed, # Vector (nyears). Number of
+#   # gridcells with eBird checklists each year
+#   nChecklist = ebird.jags.NY$nChecklist,         # Matrix (nyears x max number
+#   # of grid cells surveyed each year). Number of checklists within each grid
+#   # cell, each year
+#   Esite = ebird.jags.NY$Esite,  # Matrix (nyears x max number of grid cells
+#   # surveyed each year). Identity of checklists within each grid cell, each year
+#   nYears = ncol(ebird.jags.NY$ebird_counts), # Scalar indicating number of years
+#   # with BBS route level data
+#
+#   # Ebird checklist covariates. All numeric variables are scaled to mean 0, unit
+#   # variance.
+#   # All arrays with dim = ngridcells x nyears x nchecklists
+#   traveling = ebird.jags.NY$traveling, # Protocol type. stationary = 0,
+#   # traveling = 1.
+#   hours = ebird.jags.NY$hours,           # Duration of survey (hrs)
+#   distance = ebird.jags.NY$distance,     # Distance traveled (km)
+#   groupsize = ebird.jags.NY$group,       # Group size
+#   day = ebird.jags.NY$day,               # Day of year
+#   time = ebird.jags.NY$time,             # Time of day
+#   score = ebird.jags.NY$score,           # Observer CCI
+#   offroad = ebird.jags.NY$offroad        # Roadside = 0, off-road survey = 1
+# )
+#
+
+
 
 
 ## munge bbs for jags ------------------------------------------------------
