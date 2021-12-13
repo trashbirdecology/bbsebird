7f45891e1d9f89ac330cb8faabadbf5c70d50ebf
Jessica Burnett <jessicaleighburnett@gmail.com>
2021-12-13 08:25:45 -0700

    Fix  minor error in fxn

--- R/make_ebird_spatial.R
+++ R/make_ebird_spatial.R
@@ -1,55 +1,56 @@
 #' Create an SF object for eBird Observations
 #'
 #' Converts eBird (preferably zero-filled data) into a simple features spatial object.
-#' @param x A data frame with columns c(latitude, longitude) and optional attributes (columns).
+#' @param df A data frame with columns c(latitude, longitude) and optional attributes (columns).
 #' @param crs.target the integer representing the target CRS.
 #' @param grid  a spatial grid over which the eBird data will be overlaid.
 #' @export make_ebird_spatial
 #'
-make_ebird_spatial <- function(x, crs.target, grid = NULL) {
+make_ebird_spatial <- function(df, crs.target, grid = NULL) {
   ## need to add messages for when following assers fail
   assertthat::assert_that(is.numeric(crs.target)) ||
     assertthat::assert_that(is.integer(crs.target))
-  assertthat::assert_that(any(c("tbl_df", "tbl", "data.frame") %in% class(x)))
+  assertthat::assert_that(any(c("tbl_df", "tbl", "data.frame") %in% class(df)))
 
   # convert ebird data frame to spatial object
-  cat("assigning coordinates to `x`. takes a  minute for a few states' worth of ebird data.\n")
+  cat("assigning coordinates to `df`. takes a  minute for a few states' worth of ebird data.\n")
 
   # add attribute comprising the ebird coordinates. when i assign lat and lon as coordinates to the sf object, i lose them as attributes and
   ### theyre hard to recover when it becomes an SFC object... annnoying? yes.
-  x$lon=x$longitude
-  x$lat=x$latitude
-  coordinates(x) <-
-    ~ longitude + latitude # 1-2 minutes for all of N.Amer.
+  df <- df %>%
+    rename(lon=longitude,
+           lat=latitude)
+
+  coordinates(df) <-
+    ~ lon + lat # 1-2 minutes for all of N.Amer.
 
 
   # define projection for lat long (ebird documentation states CRS is 4326)
-  proj4string(x) <-
+  proj4string(df) <-
     CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0")
 
   # transform spatial object to sf
-  cat("coercing eBird data to sf object. \n may take between 2 and 20 minutes (rofl u lyk that estimate?!)\n")
-  x <- sf::st_as_sf(x)
+  cat("coercing eBird data to sf object. \n may take between 2 and 20 minutes (rofl u like that estimate?!).\n")
+  df <- sf::st_as_sf(df)
 
   # match proj to target proj
   cat("projecting or re-projecting the eBird data to match crs.target. \ntakes ~1 minute.\n")
-  x <-
-    st_transform(x, crs = CRS(paste0("+init=epsg:", crs.target)))
+  df <-
+    st_transform(df, crs = CRS(paste0("+init=epsg:", crs.target)))
 
   ## Exit function if no grid is provided
   if (is.null(grid))
-    return(x)
+    return(df)
 
   cat(
     "overlaying eBird and the spatial sampling grid. \ntakes ~1-2 min for a few states/provinces.\n"
   )
   # tic()
-  # ebird_spatial <- st_join(x, grid)
+  # ebird_spatial <- st_join(df, grid)
   #   grid %>%
-  #   st_join(x)
+  #   st_join(df)
   # # toc()
-  ebird_spatial <- st_join(grid, x) # should produce empty cells where no counts exist for that cell
-
+  ebird_spatial <- st_join(grid, df) # should produce empty cells where no counts exist for that cell
 
   return(ebird_spatial)
 
