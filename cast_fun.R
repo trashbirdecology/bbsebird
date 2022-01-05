3ccf030581e2757de8d3857c3351c3396ffdbc56
Jessica Burnett <jessicaleighburnett@gmail.com>
2022-01-05 07:39:18 -0700

    Create cast_fun.R

---
+++ cast_fun.R
@@ -0,0 +1,32 @@
+#' @title Cast Data to Array or Matrix for Use in JAGS
+#' @param df Data frame with three or more columns
+#' @param row target rownames
+#' @param col target colnames
+#' @param slice target slice-by values
+#' @param val target value to populate the matri(ces)
+#' @param include.na.vals TRUE default. If FALSE will drop the values of arg "val" where is NA.
+#' @importFrom dplyr distinct
+#' @export cast_fun
+cast_fun <- function(df=bbs, 
+                     row="rteno", 
+                     col="year",
+                     slice="gridcellid",
+                     val, 
+                     include.na.vals = TRUE
+                     ){
+  keep <- c(row, col, slice, val, NA, "NULL", "NA", NULL)
+  keep <- na.omit(keep[!grepl(paste0(c("NULL","NA", NA, NULL), collapse = "|"), keep)])
+  df <- df[,keep]
+  df <- dplyr::distinct(df)
+  
+  if(!include.na.vals) df <- df %>% na.omit(val)
+  
+  expr <- paste(strsplit(names(df)[-ncol(df)], split = "#"),
+        collapse = "~",
+        sep="#")
+  
+  df.new <- acast(df, eval(parse(text=expr)), value.var = val)
+  
+  return(df.new)
+  
+}
