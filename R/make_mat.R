74e297d58dbb6eecb45d47ed3df108434500c079
Jessica Burnett <jessicaleighburnett@gmail.com>
2022-01-05 14:49:23 -0700

    add arg to fill na with zero

--- R/make_mat.R
+++ R/make_mat.R
@@ -4,24 +4,27 @@
 #' @param row Variable in dat containing target row names
 #' @param col Variable in dat containig target column names
 #' @param val Variable containing the target cell contents
+#' @param replace.na Whether to replace NA values with zero.
 #' @export make_mat
 
-make_mat <- function(dat, row="rteno", col="gridcellid", val) {
-  
+make_mat <- function(dat, row="rteno", col="gridcellid", val, replace.na=FALSE) {
+
   # names <- names(dat)
-  ## will make row and col NULL and then add a thing for when they are NULL for ebird and bbs 
+  ## will make row and col NULL and then add a thing for when they are NULL for ebird and bbs
   # e.g. if(is.null(row) & "rteno" %in% names) row <- "rteno"
-  
-  mat <- tidyr::pivot_wider(dat, 
-                            id_cols=row, 
-                            names_from=col, 
-                            values_from=val) %>% 
+  mat <- tidyr::pivot_wider(dat,
+                            id_cols=row,
+                            names_from=col,
+                            values_from=val) %>%
     as.data.frame() ## add this instead of matrix otherwise numeric cell values --> character
   # make first col the rownames
   rownames(mat) <- mat[,1]
   # remove it
   mat <- mat[,-1]
-  
+
+
+  # if replace.na is TRUE, then supply NAs with zeroes
+  if(replace.na) mat[is.na(mat)] <- 0
   return(mat)
-  
+
   }
