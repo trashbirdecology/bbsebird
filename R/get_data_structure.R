ae154a161a9654e4d2edb486a11c94ff76aa9118
Jessica Burnett <jessicaleighburnett@gmail.com>
2022-01-05 10:42:34 -0700

    update get_data_struc

--- R/get_data_structure.R
+++ R/get_data_structure.R
@@ -1,18 +1,19 @@
-#' Get lista Structure of JAGS lista Inputs
+#' Get Data Structure of JAGS List Inputs
 #' @param list A list of named objects.
 #' @param filename If specified saves the resulting metadata table to file as .csv
 #' @export get_data_structure
-get_data_structure <- function(list, filename=NULL){
+get_data_structure <- function(list){
 for(i in seq_along(list)){
   if(i == 1 ){
-    output = lista.frame(matrix(nrow=max(seq_along(list)), ncol=5))
+    output = matrix(nrow=max(seq_along(list)), ncol=5)
     colnames(output) = c("class", "length", "nrow", "ncol", "nslice")
   }
-  list   <-  list[[i]]
-  class <-  class(list)[1] # for multiple classifications will just take the first
-  nrow   = dim(list)[1] #num cols
-  ncol   = dim(list)[2] #num rows
-  nslice = dim(list)[3] #num slices
+  temp   <-  list[[i]]
+  if(is.factor(temp)) temp <- as.character(temp)
+  class  <-  class(temp)[1] # for multiple classifications will just take the first
+  nrow   = dim(temp)[1] #num rows
+  ncol   = dim(temp)[2] #num cols
+  nslice = dim(temp)[3] #num slices
   # if all those are NA, then its a single value.
   if(all(is.na(c(nrow, ncol, nslice)))){length=length(list)}else{length=NA}
   if(is.null(nrow))nrow=NA
@@ -24,10 +25,8 @@ for(i in seq_along(list)){
   output[i,3] <- nrow
   output[i,4] <- ncol
   output[i,5] <- nslice
-  rownames(output)[i] <-  names(list)[i]
 }
-
-if(!is.null(filename)) write.csv(x = output, file = filename)
+rownames(output) <- names(list)
 
 return(output)
 }
