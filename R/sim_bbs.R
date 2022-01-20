8062b758bc2953c8d8817c7025f4d24c3f141f5f
Jessica Burnett <jessicaleighburnett@gmail.com>
2022-01-20 15:54:13 -0700

    Finalzie simulated bbs data

--- R/sim_bbs.R
+++ R/sim_bbs.R
@@ -1,6 +1,6 @@
 #' Simulate BBS Data for Use in JAGS
 #'
-#' Creates a list of BBS data for use in JAGS
+#' Creates a list of BBS data for use in JAGS. Data is simulated using Poisson or zero-inflated Poisson distributions. Output includes both N and C.
 #' @param propna proportion of all routes by year combinations to have no data (NA). this is a random process so will not always equal propna (uses rbinom to generate NAs)
 #' @param propfyer proportion of routes by year combinations that comprise an observer's first year
 #' @param nsite number of routes
@@ -8,8 +8,11 @@
 #' @param ngrid number of grid cells
 #' @param zip TRUE will generate N using a zero-inflated poisson distribution, FALSE non-zip
 #' @param maxN max number in the true state
+#' @param maxgridperroute max number of grids that a single route can span. use whatever makes most sense. if probMinMultG is very low, then use 1
 #' @param probMinMultG probability of a route falling into multiple grid cells. probMinMultG should decrease as grid cell size increases (i.e., less chance of a rotue falling into multiple grid cells as spatial coverage of a cell increases)
-sim_bbs <- function(nyear=20, nsite=10, ngrid=3, zip=TRUE, maxN=33, probMinMultG=0.75, probfyear=0.15, propna=0.25){
+sim_bbs <- function(nyear=20, nsite=10, ngrid=10, zip=TRUE, maxN=33, probMinMultG=0.75, probfyear=0.15, propna=0.25, maxgridperroute=5){
+
+stopifnot(maxgridperroute < ngrid)
 
 T    <- nyear
 G    <- ngrid
@@ -17,12 +20,29 @@ M    <- nsite
 
 # Simulate routes falling in multiple grids
 ## create a site by grid matrix for prop. route in a grid
-prob <- prop <- matrix(NA, nrow=M, ncol=G)
+prop <- matrix(NA, nrow=M, ncol=G)
+ind  <- vector(length=M)
 for(j in 1:M){
-  # for(i in 1:G){
-  prob[j,] <- rbinom(n=G, size=1, prob = probMinMultG) #vector
+  ## create an index for whether or not a given route falls into multiple grids
+  ind[j] <- rbinom(n=1, size=1, prob = probMinMultG)
+
+  ## if route falls into on grid cell, randomly assign a 1.00 (100%) into a grid cell
+  if(ind[j]==0){ prop[j, sample(1:G, size=1, replace=FALSE)] <- 1.00
+  next()
+  }
+
+  ## for each route that spans multiple grids, we need to randomly grab X consecutive (to mimic spatial neighbors) grid cells to assign to
+  g.ind = sample(2:maxgridperroute, size = 1) # numb grids to assign values to
+  g.ids = sort(sample(1:G, size=g.ind, replace=FALSE))
+
+  ### create a vectr of proportions for each grid cell id (g.ids)
+  props = runif(length(g.ids),0, 1)
+  props = (props+abs(min(props)))/sum(props+abs(min(props)))
+  ### assign these proportions to row j prop[j, ]
+  prop[j,g.ids] <- props
 }
-## i think that because we supplied G as n in rbinom, each rowsum shuld be >0
+prop[is.na(prop)] <- 0 # force NAs to zero.
+
 
 # Simulate true state (N)
 N <- matrix(nrow=M, ncol=T)
