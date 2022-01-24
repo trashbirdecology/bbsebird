73c39bd68b3874a6cc19b268f56a31faf851636c
Jessica Burnett <jessicaleighburnett@gmail.com>
2022-01-24 14:55:13 -0700

    Update get_data_structure.R

--- R/get_data_structure.R
+++ R/get_data_structure.R
@@ -9,9 +9,13 @@ for(i in seq_along(list)){
     colnames(output) = c("class", "length", "nrow", "ncol", "nslice")
   }
 
+
   temp   <-  list[[i]]
+suppressWarnings(  if(class(temp)=="units") temp <- units::drop_units(temp))
   if(is.factor(temp)) temp <- as.character(temp)
   class  <-  class(temp)[1] # for multiple classifications will just take the first
+  if(any(c("numeric", "integer") %in% class)) class <- "vector"
+  if(class %in% c("tbl_df", "data.frame", "tibble")) class <- "data frame"
   nrow   = dim(temp)[1] #num rows
   ncol   = dim(temp)[2] #num cols
   nslice = dim(temp)[3] #num slices
@@ -20,8 +24,7 @@ for(i in seq_along(list)){
   if(is.null(nrow))nrow=NA
   if(is.null(ncol))ncol=NA
   if(is.null(nslice))nslice=NA
-  if(any(c("array", "data.frame", "tbl_df
-") %in% class)) length <- NA
+  if(any(c("array", "data.frame", "tbl_df") %in% class)) length <- NA
 
   output[i,1] <- class
   output[i,2] <- length
@@ -30,6 +33,10 @@ for(i in seq_along(list)){
   output[i,5] <- nslice
 }
 rownames(output) <- names(list)
+
+# sort output
+output <- output[order(rownames(output)),]
+
 
 if(is.null(dir.output)) dir.output <- "/"
 fn=paste0(dir.output,"jdat-structure" ,".csv")
