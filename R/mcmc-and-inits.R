4bdf42059d19f91e0c4099ae896a1389af681987
Jessica Burnett <jessicaleighburnett@gmail.com>
2022-02-08 15:12:59 -0700

    Combine mcmc and inits into a single .r file

--- R/mcmc-and-inits.R
+++ R/mcmc-and-inits.R
@@ -23,3 +23,20 @@ set_mcmc_specs <- function(na=1000,
               ))
 
 }
+
+
+
+#' Create a List of Repeated Initial Values for each Chain
+#'
+#' @param inits a list of initial values which will be repeated according to arg `nc`
+#' @param nc number of chains (number of times the inits will be repeated)
+#' @export make_inits_list
+make_inits_list <- function(inits, nc = 3) {
+  inits.out <- list()
+  for (i in 1:nc) {
+    inits.out[[i]] <- inits
+  }
+  stopifnot(length(inits.out) == nc)
+  return(inits.out)
+
+}
