eae76d5b246eb421a063d3dd07d47250ace69f67
Jessica Burnett <jessicaleighburnett@gmail.com>
2022-03-28 15:19:50 -0600

    DOCS

--- R/utils-longlat_to_utm.R
+++ R/utils-longlat_to_utm.R
@@ -4,54 +4,54 @@
 #' @source https://stackoverflow.com/questions/18639967/converting-latitude-and-longitude-points-to-utm
 #' @export find_UTM_zone
 find_UTM_zone <- function(longitude, latitude) {
-  
+
   # Special zones for Svalbard and Norway
-  if (latitude >= 72.0 && latitude < 84.0 ) 
-    if (longitude >= 0.0  && longitude <  9.0) 
+  if (latitude >= 72.0 && latitude < 84.0 )
+    if (longitude >= 0.0  && longitude <  9.0)
       return(31);
   if (longitude >= 9.0  && longitude < 21.0)
     return(33)
   if (longitude >= 21.0 && longitude < 33.0)
     return(35)
-  if (longitude >= 33.0 && longitude < 42.0) 
+  if (longitude >= 33.0 && longitude < 42.0)
     return(37)
-  
+
   (floor((longitude + 180) / 6) %% 60) + 1
 }
 
 
 # LL to UTM ---------------------------------------------------------------
 #' Convert long, lat Locations to UTM coordinates and zones
-#' Returns a data frame containing the UTM values, the zone and the hemisphere associated with long lat coordinates. 
+#' Returns a data frame containing the UTM values, the zone and the hemisphere associated with long lat coordinates.
 #' @param long vector of X coordinates
 #' @param units one of c('m', 'km') returns UTM coordinates in meters or kilometers, respectively.
 #' @param lat vector of Y coordinates
 #' @export longlat_to_UTM
 
 longlat_to_UTM <- function(long, lat, units = 'm') {
-  
+
   df <- data.frame(
-    id = seq_along(long), 
-    x = long, 
+    id = seq_along(long),
+    x = long,
     y = lat
   )
   sp::coordinates(df) <- c("x", "y")
-  
+
   zone <- find_UTM_zone(long, lat)
-  
-  sp::proj4string(df) <- sp::CRS("+init=epsg:4326") 
-  
+
+  sp::proj4string(df) <- sp::CRS("+init=epsg:4326")
+
   hemisphere <- ifelse(lat > 0, "north", "south")
-  
+
   CRSstring <- paste0(
     "+proj=utm +zone=", zone,
     " +ellps=WGS84",
     " +", hemisphere,
     " +units=", units)
-  
-  # if (dplyr::n_distinct(CRSstring) > 1L) 
+
+  # if (dplyr::n_distinct(CRSstring) > 1L)
   #   stop("multiple zone/hemisphere detected")
-  
+
   nzones <- unique(zone)
   # sort by zone
   out <- list()
@@ -61,27 +61,27 @@ longlat_to_UTM <- function(long, lat, units = 'm') {
     dat = df[zrows,]
     h = hemisphere[i]
 
-    
+
     ids = dat$id
   out[[i]] <- sp::spTransform(dat, sp::CRS(CRSstring[1L])) %>%
     tibble::as_data_frame() %>%
     dplyr::mutate(
       zone = z,
-      hemisphere = h, 
+      hemisphere = h,
       id = ids
     )
 
   } # end loop
-  
+
   # browser()
-  coords.out <- dplyr::bind_rows(out) |> 
+  coords.out <- dplyr::bind_rows(out) |>
     dplyr::arrange(id) |>
     dplyr::mutate(
-      long = long, 
+      long = long,
       lat  = lat
     ) |>
     dplyr::select(-id)
-  
-  return(coords.out)  
+
+  return(coords.out)
 }
 
