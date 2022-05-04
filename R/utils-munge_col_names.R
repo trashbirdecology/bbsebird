452c425dafd4a7f4f305bbc397ae5e0d84f86188
Jessica Burnett <jessicaleighburnett@gmail.com>
2022-05-04 14:14:48 -0600

    Fix alt lon names ine bird munging col naes

--- R/utils-munge_col_names.R
+++ R/utils-munge_col_names.R
@@ -2,14 +2,14 @@
 #' @param data flat data object with column names
 #' @export munge_col_names
 munge_col_names <- function(data){
-  
+
 names(data) <- tolower(gsub(x = names(data),pattern = " ", replacement = "_"))
 
 col_names <- list(
   date = c("observation_date", "date"),
   c =    c("observation_count", "count", "routetotal"),
-  lat  = c("lati", "latitude"),
-  lon = c("longitude", "long")
+  lat  = c("lati", "latitude", "cell.lat.centroid", "cell.lati.centroid"),
+  lon = c("longitude", "long", "cell.lon.centroid", "cell.long.centroid")
 )
 for(i in seq_along(col_names)){
   oldnames <- col_names[[i]]
